6c855e64a2c886c959f5afa954245dc7b30e9ed7
Abhishek Yadav <monuy8830@gmail.com>
2025-03-20 12:09:57 +0530

    update coupon component to display discount values in Indian Rupees

--- store/src/components/coupon/Coupon.js
+++ store/src/components/coupon/Coupon.js
@@ -62,7 +62,7 @@ const Coupon = ({ couponInHome }) => {
                   <h6 className="pl-1 text-base font-medium text-gray-600">
                     <span className="text-lg md:text-xl lg:text-xl text-red-500 font-bold">
                       {coupon?.discountType?.type === "fixed" ? (
-                        <span>${coupon?.discountType?.value}</span>
+                        <span>₹{coupon?.discountType?.value}</span>
                       ) : (
                         <span>{coupon?.discountType?.value}%</span>
                       )}
@@ -202,7 +202,7 @@ const Coupon = ({ couponInHome }) => {
                 <h2 className="pl-1 text-base font-medium text-gray-600">
                   <span className="text-lg md:text-xl lg:text-xl text-red-500 font-bold">
                     {coupon?.discountType?.type === "fixed" ? (
-                      <span>${coupon?.discountType?.value}</span>
+                      <span>₹{coupon?.discountType?.value}</span>
                     ) : (
                       <span>{coupon?.discountType?.value}%</span>
                     )}
